b84018ce85e010e105f42648d12a7a5bed713e62
FG Ribreau <github@fgribreau.com>
2010-09-10 23:30:19 +0200

    Test suite updated (using QUnit)

--- TestAndBench/test.js
+++ TestAndBench/test.js
@@ -1,20 +1,21 @@
 module("getPath");
+
+test('Specific selectors', function(){
+    equals($(document).getPath(), 'jQueryPath_document', '$(document).getPath()');
+    equals($.getPath($(document)), 'jQueryPath_document', '$.getPath($(document))');
+    equals($(window).getPath(), 'jQueryPath_window', '$(window).getPath()');
+    equals($.getPath($(window)), 'jQueryPath_window', '$.getPath($(window))');
+});
 	
 test('Selectors', function(){
-	var $elmts = $('body').add('*').add(document).add(window)
+	var $elmts = $('body').add('*')
 	  , i = $elmts.length;
 
 	while(i--){
 		$elmts.eq(i).attr('data-rel',i);
 		
-		var path = $elmts.eq(i).getPath(), $el;
-
-		if(path === 'jQueryPath_document')
-			$el = $(document);
-		else if(path === 'jQueryPath_window')
-			$el = $(window);
-		else
-			$el = $(path);
+		var path = $elmts.eq(i).getPath()
+		  , $el = $(path);
 
 		ok($el && $el.length == 1 && $el.attr('data-rel') == i, path);
 	}
